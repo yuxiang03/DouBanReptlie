1261397c34f1a34355be3814bb87fe5dd53e7c7e
vagrant <vagrant.yuxiang@gmail.com>
2022-04-05 17:12:16 +0800

    Add a for loop to solve the bug that can only crawl one page

--- src/test/java/reptileTest.java
+++ src/test/java/reptileTest.java
@@ -17,39 +17,41 @@ import java.net.URL;
 public class reptileTest {
     @Test
     public void testadd() throws IOException {
-        String url = "https://movie.douban.com/top250";
-        Document document = Jsoup.parse(new URL(url), 10000);
-        Element content = document.getElementById("wrapper");
-        Elements imgs = content.getElementsByTag("img");
+        for (int i = 0;i<226;i += 25) {
+            String url = "https://movie.douban.com/top250?start="+i+"&filter=";
+            Document document = Jsoup.parse(new URL(url), 10000);
+            Element content = document.getElementById("wrapper");
+            Elements imgs = content.getElementsByTag("img");
 
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
-        //获取SqlSession对象
-        SqlSession sqlSession = sqlSessionFactory.openSession(true);  //true表示自动提交事务
+            //获取SqlSession对象
+            SqlSession sqlSession = sqlSessionFactory.openSession(true);  //true表示自动提交事务
 
-        //获取Mapper接口的代理对象
-        HtmlParseMapper htmlParseMapper = sqlSession.getMapper(HtmlParseMapper.class);
-        //4.执行方法
+            //获取Mapper接口的代理对象
+            HtmlParseMapper htmlParseMapper = sqlSession.getMapper(HtmlParseMapper.class);
+            //4.执行方法
 
 
-        Lottery lottery = new Lottery();
+            Lottery lottery = new Lottery();
 
-        for (Element img : imgs) {
-            String src = img.attr("src");
-            String alt = img.attr("alt");
-            lottery.setVideoName(alt);
-            lottery.setAddress(src);
+            for (Element img : imgs) {
+                String src = img.attr("src");
+                String alt = img.attr("alt");
+                lottery.setVideoName(alt);
+                lottery.setAddress(src);
 
-            htmlParseMapper.parseBidding(lottery);
+                htmlParseMapper.parseBidding(lottery);
+            }
+            //获取SqlSessionFactory
+
+
+            System.out.println(lottery.getId());
+            //5.释放资源
+            sqlSession.close();
         }
-        //获取SqlSessionFactory
-
-
-        System.out.println(lottery.getId());
-        //5.释放资源
-        sqlSession.close();
 
 
     }
