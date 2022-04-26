f680127872617c5f9c538f444ce369dc12d3db1d
vagrant <vagrant.yuxiang@gmail.com>
2022-04-26 14:59:02 +0800

    Complete the optimization of redundant code

--- src/main/java/com/example/reptile/Runnable/MyRunnable.java
+++ src/main/java/com/example/reptile/Runnable/MyRunnable.java
@@ -1,20 +1,7 @@
 package com.example.reptile.Runnable;
 
 import com.example.reptile.function.Method;
-import com.example.reptile.mapper.HtmlParseMapper;
-import com.example.reptile.pojo.Lottery;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 
 public class MyRunnable implements Runnable{
     @Override
--- src/main/java/com/example/reptile/utils/HtmlParseUtil.java
+++ src/main/java/com/example/reptile/utils/HtmlParseUtil.java
@@ -1,32 +1,54 @@
 package com.example.reptile.utils;
 
+import com.example.reptile.mapper.HtmlParseMapper;
 import com.example.reptile.pojo.Lottery;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class HtmlParseUtil {
-    public List<Lottery> parseBidding() throws IOException {
-        String url = "https://movie.douban.com/top250";
+    public static void Method(int i) throws IOException {
+        String url = "https://movie.douban.com/top250?start=" + i + "&filter=";
         Document document = Jsoup.parse(new URL(url), 10000);
         Element content = document.getElementById("wrapper");
         Elements imgs = content.getElementsByTag("img");
 
-        List<Lottery> list = new ArrayList<Lottery>();
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);  //true表示自动提交事务
+
+        //获取Mapper接口的代理对象
+        HtmlParseMapper htmlParseMapper = sqlSession.getMapper(HtmlParseMapper.class);
+
+        Lottery lottery = new Lottery();
+
         for (Element img : imgs) {
             String src = img.attr("src");
             String alt = img.attr("alt");
-            Lottery user = new Lottery();
-            user.getVideoName(alt);
-            user.setAddress(src);
-            list.add(user);
+            if (src.length() == 110) {
+ //将https://img3.doubanio.com/f/movie/a02f6ed325fc52e220f299d51e730c422e2bcd16/pics/movie/douban_app_ad/qrcode.png排除
+                break;
+            } else {
+                lottery.setVideoName(alt);
+                lottery.setAddress(src);
+            }
+            htmlParseMapper.parseBidding(lottery);
         }
-        return list;
+
+        System.out.println(lottery.getId());
+        //5.释放资源
+        sqlSession.close();
     }
 }
